0e287610bd83f89ce3effef6158fca984296fc78
JjAnaya <13680130@itcuautla.edu.mx>
2018-05-17 11:32:37 -0700

    styles now are scss(node sass installed), header and social-icons were created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SocialIconsComponent } from './components/social-icons/social-icons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SocialIconsComponent
   ],
   imports: [
     BrowserModule
